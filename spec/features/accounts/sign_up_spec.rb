e283da36432ffd48c45c1c31ee371d109412e7ca
Iggy <igoririanto@rocketmail.com>
2017-11-09 17:45:42 -0800

    added subdomains to accounts

--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -2,12 +2,21 @@ require "rails_helper"
 feature "Accounts" do
   scenario "creating an account" do
     visit root_path
-#    save_and_open_page
     click_link "Create a new account"
     fill_in "Name", with: "Test"
+    fill_in "Subdomain", with: "test"
     fill_in "Email", with: "test@example.com"
     fill_in "Password", with: "password", exact: true
     fill_in "Password confirmation", with: "password"
     click_button "Create Account"
+
+    within(".flash_notice") do
+      success_message = "Your account has been created."
+      expect(page).to have_content(success_message)
+    end
+
+    expect(page).to have_content("Signed in as test@example.com")
+    expect(page.current_url).to eq("http://test.lvh.me/")
   end
+
 end
